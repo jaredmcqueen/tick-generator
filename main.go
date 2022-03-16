bdb5cb2f3d113f27b33e8ea12faa4e02acb1a857
Jared McQueen <jaredmcqueen@gmail.com>
2022-03-16 16:26:39 -0400

    jgm

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strconv"
+	"sync/atomic"
 	"syscall"
 	"time"
 
@@ -22,98 +24,107 @@ type tick struct {
 	Close  float64   `json:"close"`
 }
 
-// generateTicks uses TS.MADD to send random ticks to redis
-func generateTicks(symbolSet map[string]float64) {
-	log.Println("starting tick generator")
-	ticker := time.NewTicker(1 * time.Second)
+var counter int32
 
-	// TS.MADD requires timeseries keys before use
-	// redis commands look like TS.CREATE ticks:abc:close
-	for s := range symbolSet {
-		rdb.Do(
-			ctx,
-			"TS.CREATE",
-			fmt.Sprintf("ticks:%s:close", s),
-			"LABELS",
-			"type",
-			"tick",
-		)
+// generateTicks uses TS.MADD to send random ticks to redis
+func generateTicks() {
+	symbolCount := 1000
+	symbolSet := make(map[string]float64)
+
+	for len(symbolSet) < symbolCount {
+		symbolSet[util.RandomSymbol()] = float64(100)
 	}
 
+	log.Println("starting tick generator")
+	// ticker := time.NewTicker(1 * time.Second)
+
 	for {
-		select {
-		case t := <-ticker.C:
-			// send pre-computed values to nats immediately
-			var cmds []interface{}
-			cmds = append(cmds, "TS.MADD")
-			for k, v := range symbolSet {
-				tt := tick{
-					Time:   t,
-					Symbol: k,
-					Close:  v,
-				}
+		// select {
+		// case t := <-ticker.C:
 
-				cmds = append(cmds,
-					fmt.Sprintf("ticks:%s:close", tt.Symbol),
-					fmt.Sprintf("%d", tt.Time.UnixMilli()),
-					fmt.Sprintf("%f", tt.Close),
-				)
-			}
-			log.Println("sent", len(symbolSet), "ticks")
-			rdb.Do(ctx, cmds...)
+		// create a redis pipeline to reduce network traffic
+		// pipe := rdb.Pipeline()
+		// var values map[string]string
 
-			// compute the next batch of ticks
-			for k, v := range symbolSet {
-				symbolSet[k] = util.RandomPrice(v)
+		// iterate through all the values in the symbolSet
+		for k, v := range symbolSet {
+			// enqueue an XADD operation on the pipeline
+
+			// log.Println(k, v, t)
+			// values["t"] = t
+			// values["symbol"] = k
+			// values["close"] = strconv.FormatFloat(v, 'f', -1, 64)
+			values := map[string]string{
+				"uno":    "dos",
+				"symbol": k,
+				"close":  strconv.FormatFloat(v, 'f', -1, 64),
 			}
+
+			// log.Println(k, v)
+			_ = values
+
+			// pipe.XAdd(ctx, &redis.XAddArgs{
+			// 	Stream: "ticks",
+			// 	ID:     "*",
+			// 	Values: values,
+			// })
+			symbolSet[k] = util.RandomPrice(v)
+			atomic.AddInt32(&counter, 1)
 		}
+
+		// log.Println((t.UnixMilli()))
+		// start := time.Now()
+		// pipe.Exec(ctx)
+		// log.Printf("XADD pipeline for %v ticks took %v\n", len(symbolSet), time.Since(start))
 	}
 
 }
 
 func main() {
 	// load config
-	config, err := util.LoadConfig(".")
-	if err != nil {
-		log.Fatal("cannot load configuration", err)
-	}
+	// config, err := util.LoadConfig(".")
+	// if err != nil {
+	// 	log.Fatal("cannot load configuration", err)
+	// }
 
 	// catch control+c
 	sigsChan := make(chan os.Signal, 1)
 	signal.Notify(sigsChan, syscall.SIGINT, syscall.SIGTERM)
 
-	log.Println("connecting to redis endpoint", config.RedisEndpoint)
-	rdb = redis.NewClient(&redis.Options{
-		Addr: config.RedisEndpoint,
-	})
+	// log.Println("connecting to redis endpoint", config.RedisEndpoint)
+	// rdb = redis.NewClient(&redis.Options{
+	// 	Addr: config.RedisEndpoint,
+	// })
 
 	// test redis connection
-	_, err = rdb.Ping(ctx).Result()
-	if err != nil {
-		log.Fatal("error", err)
-	}
-	log.Println("successfully connected to", config.RedisEndpoint)
+	// _, err = rdb.Ping(ctx).Result()
+	// if err != nil {
+	// 	log.Fatal("error", err)
+	// }
+	// log.Println("successfully connected to", config.RedisEndpoint)
 
 	// clear out the db
 	// TODO: make this an envar
-	rdb.FlushAll(ctx)
-
-	// symbolCount is defined in config
-	symbolCount := config.SymbolCount
-	symbolSet := make(map[string]float64)
+	// rdb.FlushAll(ctx)
 
 	// fully populate N random symbols
 	// 100 is the starting value
-	for len(symbolSet) < symbolCount {
-		symbolSet[util.RandomSymbol()] = float64(100)
-	}
+
+	go func() {
+		for {
+			time.Sleep(1 * time.Second)
+			log.Println(counter)
+			counter = int32(0)
+		}
+	}()
 
 	// start the generator
-	go generateTicks(symbolSet)
+	for i := 0; i < 1; i++ {
+		go generateTicks()
+	}
 
 	<-sigsChan
 
 	fmt.Print("received termination signal")
 	os.Exit(0)
-
 }
